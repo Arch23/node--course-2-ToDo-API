8121117e09b6c5c2b5414ff404a71be56e1e5f00
Arch23 <noda.94@gmail.com>
2018-07-11 20:03:53 -0300

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -42,7 +42,7 @@ UserSchema.methods.toJSON = function () {
 //instance methods, have to user function because arrow function dont bind this keyword
 UserSchema.methods.generateAuthToken = function () {
     var access = 'auth';
-    var token = jwt.sign({ _id: this._id.toHexString(), access }, 'abc123');
+    var token = jwt.sign({ _id: this._id.toHexString(), access }, process.env.JWT_SECRET);
 
     this.tokens = this.tokens.concat([{
         access,
@@ -59,7 +59,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         return Promise.reject();
     }
